e2c92c5b5e339cfb2f4ec8f4c4a2e633ca3b5623
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2019-11-13 13:43:01 +0000

    Boom unity Project
    i hope i didnt break anything trying to upload this (it doesnt count how many importants are remaining properly)

---
+++ Boom/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 euler = transform.rotation.eulerAngles;
+        euler.x += Input.GetAxis("Vertical");
+        euler.y -= Input.GetAxis("Horizontal");
+        transform.rotation = Quaternion.Euler(euler);
+    }
+}
