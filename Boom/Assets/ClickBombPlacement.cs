9aace75ca1e7c493c39efabaf6f5b5dfe3723773
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2019-11-13 13:37:04 +0000

    Boom Unity project
    Had a bit of issue as i'm not sure how to sort out how to change the count of how many importants are remaining but it works

---
+++ Boom/Assets/ClickBombPlacement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBombPlacement : MonoBehaviour
+{
+    public GameObject bombPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePositionWithZOffset = Input.mousePosition;
+            mousePositionWithZOffset.z = 10;
+
+            Vector3 cameraPosition = Camera.main.transform.position;
+
+            Vector3 mousePositionInWorldSpace = Camera.main.ScreenToWorldPoint(mousePositionWithZOffset);
+
+            Vector3 directionFromCameraToMouse = mousePositionInWorldSpace - cameraPosition;
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(new Ray(cameraPosition, directionFromCameraToMouse), out hitInfo, Mathf.Infinity)){
+                GameObject bombInstance = Instantiate(bombPrefab);
+
+                bombInstance.transform.parent = hitInfo.collider.transform;
+
+                bombInstance.transform.position = hitInfo.point;
+
+                bombInstance.transform.localRotation = Quaternion.identity;
+
+                bombInstance.transform.localScale = Vector3.one;
+
+                Vector3 trueScale = bombInstance.transform.lossyScale;
+
+                bombInstance.transform.localScale = new Vector3(1 / trueScale.x, 1 / trueScale.y, 1 / trueScale.z);
+
+                bombInstance.GetComponent<bomb>().LightFuse();
+
+            }
+        }
+    }
+}
---
+++ Boom/Assets/Scripts/MouseScrollMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScrollMovement : MonoBehaviour
+{
+  
+    void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + Input.mouseScrollDelta.y);
+    }
+}
